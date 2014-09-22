fd661f745ef1a3ec493967646e3e9c314fb19e79
Bryant Syme <prefu@hotmail.com>
2014-09-22 01:15:23 -0700

    getting a test to deploy

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+ // creates a new httpServer instance
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('<h1>hello, i know nodejitsu.</h1>');
+  res.end();
+}).listen(8080); // the server will listen on port 8080
---
+++ spec/first.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe("first test", function() {
+  it("is awesome", function() {
+    expect(true).toBe(true);
+  });
+});
