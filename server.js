7bc71537dabe69352c97d48bca810803c500da8c
Bryant Syme <prefu@hotmail.com>
2014-10-06 19:27:39 -0700

    added another server

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var http = require('http');
 
- // creates a new httpServer instance
+// creates a new httpServer instance
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1>hello, i know nodejitsu.</h1>');
-  res.end();
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	res.write('<h1>hello, i know nodejitsu.</h1>');
+	res.end();
 }).listen(8080); // the server will listen on port 8080
+// creates a new httpServer instance
+http.createServer(function (req, res) {
+	res.writeHead(200, {
+		'Content-Type': 'text/html'
+	});
+	res.write('<h1>hello, i know nodejitsu, better and stuff.</h1>');
+	res.end();
+}).listen(8081); // the server will listen on port 8080
