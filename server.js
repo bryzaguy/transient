cfe4d01ab7d9ee250153e7b9081dec35193f1d44
Bryant Syme <prefu@hotmail.com>
2014-10-06 20:03:58 -0700

    stuff

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ http.createServer(function (req, res) {
 	res.writeHead(200, {
 		'Content-Type': 'text/html'
 	});
-	res.write('<h1>hello, i know nodejitsu.</h1>');
-	res.end();
-}).listen(8080); // the server will listen on port 8080
-// creates a new httpServer instance
-http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/html'
-	});
 	res.write('<h1>hello, i know nodejitsu, better and stuff.</h1>');
 	res.end();
 }).listen(8081); // the server will listen on port 8080
