1efc22d778cb3e0654f15ab276e3f8244d3f345b
Bryant Syme <prefu@hotmail.com>
2015-04-18 15:52:29 -0700

    added server codes

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-var http = require('http');
+var http = require('http'),
+  fs = require('fs');
 
-// creates a new httpServer instance
-http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/html'
-	});
-	res.write('<h1>hello, i know nodejitsu, better and stuff.</h1>');
-	res.end();
-}).listen(80); // the server will listen on port 8080
-// creates a new httpServer instance
-http.createServer(function (req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/html'
-	});
-	res.write('<h1>hello, i know nodejitsu, secure</h1>');
-	res.end();
-}).listen(443); // the server will listen on port 8080
+var app = http.createServer(function (request, response) {
+  fs.readFile("client.html", 'utf-8', function (error, data) {
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    response.write(data);
+    response.end();
+  });
+}).listen(1337);
